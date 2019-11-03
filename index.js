1e67d504451287722129eca4aa91bcf84890fa8f
sumitkumardey91 <sumit.kumar@mobiotics.com>
2019-11-03 21:00:20 +0530

    remove

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const path = require('path');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 var con = mysql.createConnection({
   host: "remotemysql.com",
